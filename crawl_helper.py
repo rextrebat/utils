a3f9979c75dc91716135d9f069643c26b7c64b72
Kingshuk Dasgupta <rextrebat@gmail.com>
2012-10-09 22:16:12 -0500

    added params_no_encode

--- crawl_helper.py
+++ crawl_helper.py
@@ -6,7 +6,6 @@ __author__ = "Kingshuk Dasgupta (rextrebat/kdasgupta)"
 __version__ = "0.0pre0"
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
 import urllib2
 import urllib
@@ -14,6 +13,8 @@ import json
 import BeautifulSoup
 
 import time
+import threading
+import Queue
 
 
 logger = logging.getLogger('crawl_helper')
@@ -30,7 +31,6 @@ class ResponseFormat:
 class Throttler(object):
     """
     Throttle Manager:
-    FIXME: Make this threadsafe
     """
 
     def __init__(self, limit_requests, 
@@ -39,21 +39,24 @@ class Throttler(object):
         self.limit_interval = limit_interval
         self.sleep_bw_checks = sleep_bw_checks
         self.requests = []
+        self.lock = threading.Lock()
 
     def next_request(self):
         """
         This method will return when it is ok to make the next request
         """
-        while True:
-            now = time.time()
-            t_begin = now - self.limit_interval
-            self.requests = [r for r in self.requests if r > t_begin]
-            if len(self.requests) >= self.limit_requests:
-                time.sleep(self.sleep_bw_checks)
-            else:
-                self.requests.append(now)
-                logger.debug("Throttler - request added %s" % (str(now)))
-                break
+        with self.lock:
+            while True:
+                now = time.time()
+                t_begin = now - self.limit_interval
+                self.requests = [r for r in self.requests if r > t_begin]
+                if len(self.requests) >= self.limit_requests:
+                    logger.debug("Throttling...")
+                    time.sleep(self.sleep_bw_checks)
+                else:
+                    self.requests.append(now)
+                    logger.debug("Throttler - request added %s" % (str(now)))
+                    break
 
 class HTTPRequester(object):
     """
@@ -84,7 +87,8 @@ class HTTPRequester(object):
         if self.throttler:
             self.throttler.next_request()
         try:
-            response = urllib2.urlopen(url)
+            logger.debug("Request: \n %s" % (str(url)[:1024]))
+            response = urllib2.urlopen(url).read()
             logger.debug("Response: \n %s" % (str(response)[:1024]))
             if self.response_format == ResponseFormat.RAW:
                 return response
@@ -94,5 +98,115 @@ class HTTPRequester(object):
                 return json.loads(response)
             elif self.response_format == ResponseFormat.SOUP:
                 return BeautifulSoup(response)
-        except urllib2.URLError, e:
-            logger.debug("Failure %s" % (e.reason))
+        except urllib2.URLError as e:
+            logger.error("Failure %s" % (e.reason))
+
+
+class FetcherConfig:
+    """
+    Fetcher Config
+    """
+
+    def __init__(
+            self,
+            base_url,
+            base_params={},
+            response_format=ResponseFormat.RAW,
+            ):
+        self.base_url = base_url
+        self.base_params = base_params
+        self.response_format = response_format
+
+
+class FetchTask:
+    """
+    Fetch Task
+    """ 
+    
+    def __init__(self, config, params=None, params_no_encode=None,
+            context=None, process_response=None):
+        self.config = config
+        self.params = params
+        self.params_no_encode = params_no_encode
+        self.context = context
+        self.process_response = process_response
+
+    def respond(self, response):
+        if self.config.response_format == ResponseFormat.RAW:
+            pass
+        elif self.config.response_format == ResponseFormat.STR:
+            response = str(response)
+        elif self.config.response_format == ResponseFormat.JSON:
+            response = json.loads(response)
+        elif self.config.response_format == ResponseFormat.SOUP:
+            response = BeautifulSoup(response)
+        if not self.context:
+            return self.process_response(response)
+        else:
+            return self.process_response(response, self.context)
+
+
+
+class Fetcher(threading.Thread):
+    """
+    Fetcher
+    """
+
+    def __init__(self, queue, throttlers=None):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.throttlers = throttlers
+        self.terminate = False
+
+    def fetch(self, task):
+        if task.params or task.config.base_params:
+            url = task.config.base_url + "?"
+        else:
+            url = task.config.base_url
+        url += urllib.urlencode(task.config.base_params)
+        if task.config.base_params:
+            url += "&" + urllib.urlencode(task.params)
+        else:
+            url += urllib.urlencode(task.params)
+        if task.params or task.config.base_params:
+            url += "&" + task.params_no_encode
+        else:
+            url += task.params_no_encode
+        try:
+            logger.debug("Request: \n %s" % (str(url)[:1024]))
+            response = urllib2.urlopen(url).read()
+            logger.debug("Response: \n %s" % (str(response)[:1024]))
+            return task.respond(response)
+        except urllib2.URLError as e:
+            logger.error("Failure %s" % (e.reason))
+
+    def run(self):
+        while not self.terminate:
+            try:
+                task = self.queue.get()
+                if self.throttlers:
+                    for throttler in self.throttlers:
+                        throttler.next_request()
+                self.fetch(task)
+            except Queue.Empty:
+                pass
+
+
+class FetcherPool:
+    """
+    FetcherPool
+    """
+
+    def __init__(self, size, throttlers=None):
+        self.size = size
+        self.throttlers = throttlers
+        self.queue = Queue.Queue()
+        self.fetchers = [Fetcher(
+            self.queue, self.throttlers) for i in range(size)]
+        for fetcher in self.fetchers:
+            fetcher.daemon = True
+            fetcher.start()
+
+    def stop(self):
+        for fetcher in self.fetchers:
+            fetcher.terminate = True
--- test_crawl_helper.py
+++ test_crawl_helper.py
@@ -6,7 +6,7 @@ __author__ = "Kingshuk Dasgupta (rextrebat/kdasgupta)"
 __version__ = "0.0pre0"
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 import unittest
 import mock
@@ -17,7 +17,7 @@ import time
 class TestRequester(unittest.TestCase):
 
     @mock.patch("crawl_helper.urllib2")
-    def test_throttle(self, mock_urllib2):
+    def test_throttle1(self, mock_urllib2):
         throttler = crawl_helper.Throttler(3, 1)
         requester = crawl_helper.HTTPRequester(
                 base_url="testurl",
@@ -26,6 +26,39 @@ class TestRequester(unittest.TestCase):
         for i in range(100):
             requester.get()
             time.sleep(0.1)
+
+    @mock.patch("crawl_helper.urllib2")
+    def test_throttle2(self, mock_urllib2):
+        throttler = crawl_helper.Throttler(5, 1)
+        requester = crawl_helper.HTTPRequester(
+                base_url="testurl",
+                throttler=throttler
+                )
+        for i in range(100):
+            requester.get()
+            time.sleep(0.5)
+
+    def process_response(self, s):
+        print s
+
+    @mock.patch("crawl_helper.urllib2")
+    def test_fetch(self, mock_urllib2):
+        config = crawl_helper.FetcherConfig(
+                base_url="testurl")
+        throttler1 = crawl_helper.Throttler(5, 1)
+        fetcher_pool = crawl_helper.FetcherPool(
+                size=3,
+                throttlers=[throttler1])
+        for i in range(100):
+            fetch_task = crawl_helper.FetchTask(
+                    config=config,
+                    process_response=self.process_response
+                    )
+            fetcher_pool.queue.put(fetch_task)
+        while not fetcher_pool.queue.empty():
+            time.sleep(0.1)
+        time.sleep(2)
+        fetcher_pool.stop()
 
 
 if __name__ == '__main__':
